a835446e4e121c4be4887ca4cda4263041dd1648
gnart <nvtrang91@gmail.com>
2025-02-11 13:40:45 +0700

    obpd

--- test/TestBase.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract TestBase {
-    address internal constant OWNER = address(0xabc123);
-    address internal constant ATTACKER = address(0xdef456);
-}
